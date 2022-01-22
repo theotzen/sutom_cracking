da40826befd17f54e3eb72140507da2403a8743f
theotzen <73227106+theotzen@users.noreply.github.com>
2022-01-22 17:39:40 +0100

    main and utils - all of it working

---
+++ main.py
@@ -0,0 +1,56 @@
+import utils as ut
+import pandas as pd
+
+
+from bs4 import BeautifulSoup as bs
+import numpy as np
+import pandas as pd
+from selenium import webdriver
+import time
+import pyperclip as pc
+
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.add_argument('--headless')
+
+lexique = pd.read_excel(
+    '/Users/theodoreotzenberger/Downloads/Lexique383/Lexique383.xlsb')
+
+lexique = lexique[['1_ortho', '8_freqlemlivres']].rename(
+    columns={'1_ortho': 'mot', '8_freqlemlivres': 'freq'})
+lexique.dropna(axis=0, inplace=True)
+
+browser = webdriver.Firefox()
+browser.set_window_size(1200, 900)
+already_seen = []
+
+for i in range(6):
+    browser.get('https://sutom.nocle.fr/')
+    time.sleep(2)
+    html = browser.execute_script('return document.documentElement.outerHTML')
+    all_html = bs(html, 'lxml')
+
+    grille_scd = ut.extract_grille(all_html)
+    if grille_scd[i].lower() == '':
+        print('break')
+        break
+    print(grille_scd[i].lower())
+    forbidden_scd = ut.get_forbidden_letters_from_html(all_html)
+    forbidden_scd = [letter.lower() for letter in forbidden_scd]
+    print(forbidden_scd)
+    yellow_scd = ut.get_yellow_letters_from_html(all_html)
+    yellow_scd = [letter.lower() for letter in yellow_scd]
+    print(yellow_scd)
+
+    to_try = ut.get_first_word(ut.filter_results(ut.get_most_freq_res(ut.get_results(lexique, ut.create_total_query(
+        yellow_scd, forbidden_scd, grille_scd[i].lower()))).to_frame(), already_seen))
+    print(to_try)
+
+    ut.write_word(to_try.upper(), browser)
+    already_seen.append(to_try)
+
+time.sleep(1)
+browser.find_element_by_xpath(
+    f"//a[@id='fin-de-partie-panel-resume-bouton']").click()
+print(pc.paste())
---
+++ utils.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import re
+
+from bs4 import BeautifulSoup as bs
+import numpy as np
+import requests
+import pandas as pd
+import webbrowser
+import urllib.request
+import csv
+from selenium import webdriver
+import time
+
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.add_argument('--headless')
+
+
+def create_query(current_word):
+    query = ""
+    for idx, char in enumerate(current_word):
+        if char == '.':
+            pass
+        else:
+            query += f"mot.str[{idx}] == '{char}' and "
+    return query + "mot.str.len() == " + str(len(current_word))
+
+
+def get_results(lex, query):
+    return lex.query(query, engine="python").sort_values(by='freq', ascending=False)
+
+
+def get_most_freq_res(t):
+    return t.reset_index(drop=True).loc[0:5, 'mot']
+
+
+def create_query_letters(list_of_letters):
+    query = ""
+    for idx, letter in enumerate(list_of_letters):
+        if idx < len(list_of_letters) - 1:
+            query += f"mot.str.contains('{letter}', na=False) and "
+        else:
+            query += f"mot.str.contains('{letter}', na=False)"
+
+    return query
+
+
+def create_query_forbidden_letters(list_of_forbidden_letters):
+    query = ""
+    for idx, letter in enumerate(list_of_forbidden_letters):
+        if idx < len(list_of_forbidden_letters) - 1:
+            query += f"~mot.str.contains('{letter}', na=False) and "
+        else:
+            query += f"~mot.str.contains('{letter}', na=False)"
+
+    return query
+
+
+def create_total_query(list_of_letters, list_of_forbidden_letters, current_word):
+    if list_of_letters and list_of_forbidden_letters:
+        return create_query_letters(list_of_letters) + " and " + create_query_forbidden_letters(list_of_forbidden_letters) + " and " + create_query(current_word)
+    elif list_of_letters and not list_of_forbidden_letters:
+        return create_query_letters(list_of_letters) + " and " + create_query(current_word)
+    elif not list_of_letters and list_of_forbidden_letters:
+        return create_query_forbidden_letters(list_of_forbidden_letters) + " and " + create_query(current_word)
+    else:
+        return create_query(current_word)
+
+
+def filter_results(res, already_seen: list):
+    tampon = res[(~res.mot.isin(already_seen))]
+    return tampon[(~tampon.mot.str.contains(' ', na=False)) & (~tampon.mot.str.contains('-', na=False))]
+
+
+def get_first_word(filtered_results):
+    return filtered_results.iloc[0, 0]
+
+
+def extract_words(l: list) -> list:
+    words = []
+    for to_clean in l:
+        if len(to_clean) > 2:
+            words.append(re.sub('<td>|</td>', '', to_clean))
+
+    return words
+
+
+def extract_grille(html_page):
+    root = html_page.findAll('div', {'id': "grille"})
+    root_content = re.split('<tr>|</tr>', str(root[0]))[1:-1]
+    return extract_words(root_content)
+
+
+def get_forbidden_letters_from_html(all_html):
+    forbid = []
+    l = list(all_html.findAll(
+        'div', {'class': "input-lettre lettre-non-trouve"}))
+    if l:
+        for elem in l:
+            forbid.append(str(elem)[-7])
+    else:
+        return []
+    return forbid
+
+
+def get_yellow_letters_from_html(all_html):
+    yellow = []
+    l = list(all_html.findAll(
+        'div', {'class': "input-lettre lettre-mal-place"}))
+    if l:
+        for elem in l:
+            yellow.append(str(elem)[-7])
+    else:
+        return []
+    return yellow
+
+
+def write_word(word, browser):
+    browser.get('https://sutom.nocle.fr/')
+    for char in word:
+        element = browser.find_element_by_xpath(
+            f"//div[@data-lettre='{char}']")
+        element.click()
+    browser.find_element_by_xpath(f"//div[@data-lettre='_entree']").click()
